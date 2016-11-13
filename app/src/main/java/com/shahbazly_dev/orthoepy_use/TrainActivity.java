77cf65365acf129cc1aa14a0543dca89d4ee03c0
archieDeveloper <arkadij.ok@gmail.com>
2016-11-13 22:21:46 +0000

    разделил один большой метод на несколько маленьких

--- app/src/main/java/com/shahbazly_dev/orthoepy_use/TrainActivity.java
+++ app/src/main/java/com/shahbazly_dev/orthoepy_use/TrainActivity.java
@@ -60,66 +60,81 @@ public class TrainActivity extends AppCompatActivity {
         myButton.setCornerRadius(100);
         myButton.setShadowEnabled(false);
         if (isVowelsChar) {
-            myButton.setButtonColor(getResources().getColor(R.color.color_sun_flower));
-            myButton.setShadowColor(getResources().getColor(R.color.color_orange));
-            myButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String text = (String) myButton.getText();
-                    if (text.equals(true_letter)) {
-                        myButton.setButtonColor(getResources().getColor(R.color.color_emerald));
-                        myButton.setShadowColor(getResources().getColor(R.color.color_nephritis));
-                        final int childcount = linearLayout.getChildCount();
-                        for (int i = 0; i < childcount; i++) {
-                            v = linearLayout.getChildAt(i);
-                            TranslateAnimation translateAnimation = new TranslateAnimation(0.0f, -2000.0f, 0.0f, 0.0f);
-                            translateAnimation.setStartOffset((25*i)+25/(i+1));
-                            translateAnimation.setDuration(300);
-                            translateAnimation.setFillAfter(true);
-                            if (i == childcount-1) {
-                                translateAnimation.setAnimationListener(new TranslateAnimation.AnimationListener() {
-                                    @Override
-                                    public void onAnimationStart(Animation animation) {
-
-                                    }
-
-                                    @Override
-                                    public void onAnimationEnd(Animation animation) {
-                                        linearLayout.removeAllViews();
-                                        startTrain();
-                                    }
-
-                                    @Override
-                                    public void onAnimationRepeat(Animation animation) {
-
-                                    }
-                                });
-                            }
-                            v.startAnimation(translateAnimation);
-                        }
-                    } else {
-                        trainManager.addCountError();
-                        myButton.setButtonColor(getResources().getColor(R.color.color_alizarin));
-                        myButton.setShadowColor(getResources().getColor(R.color.color_pomegranate));
-                    }
-                }
-            });
+            createVowelsButton(myButton, true_letter);
         } else {
-            myButton.setEnabled(false);
-            myButton.setButtonColor(getResources().getColor(R.color.color_silver));
-            myButton.setShadowColor(getResources().getColor(R.color.color_concrete));
+            createConsonantButton(myButton);
         }
         linearLayout = (LinearLayout) findViewById(R.id.liner);
         linearLayout.addView(myButton);
         LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) myButton.getLayoutParams();
         params.setMargins(0, 0, 10, 0); //слева, сверху, справа, снизу
         myButton.setLayoutParams(params);
-
         TranslateAnimation translateAnimation = new TranslateAnimation(2000.0f, 0.0f, 0.0f, 0.0f);
         translateAnimation.setStartOffset((25*item)+25/(item+1));
         translateAnimation.setDuration(300);
         translateAnimation.setFillAfter(true);
         myButton.startAnimation(translateAnimation);
+    }
+
+    public void createVowelsButton(final FButton myButton, final String true_letter) {
+        myButton.setButtonColor(getResources().getColor(R.color.color_sun_flower));
+        myButton.setShadowColor(getResources().getColor(R.color.color_orange));
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = (String) myButton.getText();
+                if (text.equals(true_letter)) {
+                    correctAnswer(myButton);
+                } else {
+                    inCorrectAnswer(myButton);
+                }
+            }
+        });
+    }
+
+    public void createConsonantButton(FButton myButton) {
+        myButton.setEnabled(false);
+        myButton.setButtonColor(getResources().getColor(R.color.color_silver));
+        myButton.setShadowColor(getResources().getColor(R.color.color_concrete));
+    }
+
+    public void correctAnswer(FButton myButton) {
+        myButton.setButtonColor(getResources().getColor(R.color.color_emerald));
+        myButton.setShadowColor(getResources().getColor(R.color.color_nephritis));
+        final int childСount = linearLayout.getChildCount();
+        for (int i = 0; i < childСount; i++) {
+            View v = linearLayout.getChildAt(i);
+            TranslateAnimation translateAnimation = new TranslateAnimation(0.0f, -2000.0f, 0.0f, 0.0f);
+            translateAnimation.setStartOffset((25*i)+25/(i+1));
+            translateAnimation.setDuration(300);
+            translateAnimation.setFillAfter(true);
+            if (i == childСount-1) {
+                translateAnimation.setAnimationListener(new TranslateAnimation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        linearLayout.removeAllViews();
+                        startTrain();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+            v.startAnimation(translateAnimation);
+        }
+    }
+
+    public void inCorrectAnswer(FButton myButton) {
+        trainManager.addCountError();
+        myButton.setButtonColor(getResources().getColor(R.color.color_alizarin));
+        myButton.setShadowColor(getResources().getColor(R.color.color_pomegranate));
     }
 
 }
