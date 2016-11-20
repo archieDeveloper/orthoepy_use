d60bf892a9d58c7d33142e77f4e719d60e7c83de
shahbazly-dev <shahbazly.dev@gmail.com>
2016-11-20 13:54:27 +0300

    Метод занесения слов в БД перенесен в DBHelper

--- app/src/main/java/com/shahbazly_dev/orthoepy_use/MainActivity.java
+++ app/src/main/java/com/shahbazly_dev/orthoepy_use/MainActivity.java
@@ -1,8 +1,6 @@
 package com.shahbazly_dev.orthoepy_use;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,13 +8,10 @@ import android.widget.Button;
 
 import com.stephentuso.welcome.WelcomeHelper;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
-
 public class MainActivity extends AppCompatActivity {
 
     WelcomeHelper welcomeScreen;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        loadWords();
+        dbHelper = new DBHelper(this,1);
+        dbHelper.getWritableDatabase();
     }
 
     @Override
@@ -42,37 +38,4 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         welcomeScreen.onSaveInstanceState(outState);
     }
-    protected void loadWords() {
-        InputStream inputStream;
-        String text = "words.txt";
-        byte[] buffer;
-
-        try {
-            //перевод содержания assets/words.txt в String
-            inputStream = getAssets().open(text);
-            int size = inputStream.available();
-            buffer = new byte[size];
-
-            inputStream.read(buffer);
-            inputStream.close();
-
-            //Строка со словами из words.txt
-            String str_data = new String(buffer);
-
-            //Перенос слов в базу данных
-            DBHelper dbHelper = new DBHelper(getApplicationContext(),1);
-            SQLiteDatabase database = dbHelper.getWritableDatabase();
-            ContentValues cv = new ContentValues();
-            Scanner scan = new Scanner(str_data);
-            while (scan.hasNext()) {
-                String word = scan.next();
-                cv.put("word", word);
-                database.insert("words", null, cv);
-            }
-            scan.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
